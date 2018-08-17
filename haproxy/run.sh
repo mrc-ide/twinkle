33b041096955d67c92f0d2fe201ca71dac6483ea
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-08-17 17:32:35 +0100

    Use docker networks

--- haproxy/run.sh
+++ haproxy/run.sh
@@ -1,4 +1,5 @@
 docker run --rm -it --name myproxy \
        -v ${PWD}/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
-       --network=host \
+       -p 80:80 \
+       --network=haproxy \
        haproxy:1.7
