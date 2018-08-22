9a25b98a3bff113e22660bd6fde05d9ef5c6631e
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-08-22 15:26:55 +0100

    Use htpasswd from docker image for now

--- apache/prepare.sh
+++ apache/prepare.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-htpasswd -cb auth/users rich rich
+UID=`id -u`
+GID=`id -g`
+docker run --rm \
+       --user="${UID}:${GID}" \
+       -v ${PWD}/auth:/auth \
+       httpd:2.4 \
+       htpasswd -cb /auth/users rich rich
