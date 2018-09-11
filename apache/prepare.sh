fd4a32eab182950b98ebc1c7a0fbeb7b5d8168be
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-09-11 15:17:36 +0100

    Interpolate variables into apache config

--- apache/prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-UID=`id -u`
-GID=`id -g`
-docker run --rm \
-       --user="${UID}:${GID}" \
-       -v ${PWD}/auth:/auth \
-       httpd:2.4 \
-       htpasswd -cb /auth/users rich rich
