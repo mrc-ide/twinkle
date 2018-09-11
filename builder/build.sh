87802522e263cc3b85d11987a7af91f09e933ea8
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-09-11 13:46:17 +0100

    Support for basic user management

--- builder/build.sh
+++ builder/build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 set -e
-docker build --rm --tag "mrcide/shiny-server-builder:3.5.1" .
+HERE=$(realpath $(dirname $0))
+docker build --rm --tag "mrcide/shiny-server-builder:3.5.1" $HERE
