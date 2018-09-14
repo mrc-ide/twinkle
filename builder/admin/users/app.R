33c3daf9cc0edd226b459ef2e034bff20b465897
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-09-14 16:52:40 +0100

    Move the login module into the twinkle package

--- builder/admin/users/app.R
+++ builder/admin/users/app.R
@@ -2,70 +2,21 @@ passwordfile <- Sys.getenv("APACHE_PASSWORD_FILE", "/apache_auth/users")
 groupfile <- Sys.getenv("APACHE_PASSWORD_FILE", "/apache_auth/groups")
 vault_env_file <- Sys.getenv("VAULT_ENV_FILE", "/.vault")
 
-login_ui <- function(id) {
-  ns <- shiny::NS(id)
-  shiny::tagList(
-    shiny::h3("Admin Login"),
-    shiny::textInput(ns("username"), "Username"),
-    shiny::passwordInput(ns("password"), "Password"),
-    shiny::div(
-      class = "panel-group",
-      shiny::actionButton(ns("login"), "Login", class = "btn-primary"),
-      shiny::actionButton(ns("logout"), "Logout", class = "btn-danger")),
-    shiny::textOutput(ns("login_status")))
-}
-
-
-login_server <- function(input, output, session) {
-  auth <- shiny::reactiveValues(user = NULL, groups = NULL, is_admin = FALSE)
-
-  shiny::observeEvent(
-    input$login, {
-      username <- input$username
-      if (!nzchar(username)) {
-        modal_error("Username not provided", "Error logging in")
-      } else if (!nzchar(input$password)) {
-        modal_error("Password not provided", "Error logging in")
-      } else if (!verify_password(username, input$password, passwordfile)) {
-        modal_error("Incorrect username/password", "Error logging in")
-      } else {
-        groups <- user_membership(username, groupfile)
-        auth$username <- username
-        auth$groups <- groups
-        auth$is_admin <- "dide-internal" %in% auth$groups
-        output$login_status <- shiny::renderText(
-          sprintf("Logged in as '%s', member of %s", username,
-                  paste(squote(groups)), collapse = ", "))
-        shiny::updateTextInput(session, "username", value = "")
-        shiny::updateTextInput(session, "password", value = "")
-      }
-    })
-
-  shiny::observeEvent(
-    input$logout, {
-      auth$username <- NULL
-      auth$groups <- NULL
-      auth$is_admin <- FALSE
-      output$login_status <- NULL
-    })
-
-  auth
-}
-
-
 ui <- shiny::fluidPage(
   shiny::titlePanel("User management"),
   shiny::sidebarLayout(
     shiny::sidebarPanel(
-      login_ui("login")),
+      twinkle::login_ui("login")),
     shiny::mainPanel(
       shiny::uiOutput("main"))))
 
 
 server <- function(input, output, session) {
-  set_vault_env(vault_env_file)
+  Sys.unsetenv(c("VAULTR_AUTH_METHOD", "VAULTR_CACHE_DIR"))
+  readRenviron(vault_env_file)
 
-  auth <- shiny::callModule(login_server, "login")
+  auth <- shiny::callModule(twinkle::login_server, "login",
+                            passwordfile, groupfile)
 
   shiny::observe({
     output$main <- shiny::renderUI(server_main_panel(auth$is_admin))
@@ -73,17 +24,19 @@ server <- function(input, output, session) {
 
   shiny::observeEvent(
     input$pw_set, {
+      error_title <- "Error setting password"
       if (!nzchar(input$pw_usr)) {
-        modal_error("Username not provided", "Error setting password")
+        twinkle::modal_error("Username not provided", error_title)
       } else if (!nzchar(input$pw_new)) {
-        modal_error("Password must not be blank", "Error setting password")
+        twinkle::modal_error("Password must not be blank", error_title)
       } else if (!identical(input$pw_new, input$pw_chk)) {
-        modal_error("New passwords do not match", "Error setting password")
+        twinkle::modal_error("New passwords do not match", error_title)
       } else if (!isTRUE(auth$is_admin) &&
-                  !verify_password(input$pw_usr, input$pw_old, passwordfile)) {
-        modal_error("Incorrect old password", "Error setting password")
+                  !twinkle::verify_password(input$pw_usr, input$pw_old,
+                                            passwordfile)) {
+        twinkle::modal_error("Incorrect old password", error_title)
       } else {
-        update_user_password(username, password, passwordfile)
+        twinkle::update_user_password(username, password, passwordfile)
         shiny::updateTextInput(session, "pw_usr", value = "")
         shiny::updateTextInput(session, "pw_old", value = "")
         shiny::updateTextInput(session, "pw_new", value = "")
@@ -95,7 +48,8 @@ server <- function(input, output, session) {
 
 server_main_panel <- function(is_administrator) {
   if (is_administrator) {
-    pw_usr <- shiny::selectInput("pw_usr", "Username", read_users(passwordfile))
+    pw_usr <- shiny::selectInput("pw_usr", "Username",
+                                 twinkle::read_users(passwordfile))
   } else {
     pw_usr <- shiny::textInput("pw_usr", "Username")
   }
@@ -106,98 +60,6 @@ server_main_panel <- function(is_administrator) {
     shiny::passwordInput("pw_new", "New password"),
     shiny::passwordInput("pw_chk", "Confirm new password"),
     shiny::actionButton("pw_set", "Set password", class = "btn-success"))
-}
-
-
-
-## Most of the rest of this can/should go into the twinkle package I
-## think.
-modal_error <- function(msg, title) {
-  shiny::showModal(shiny::modalDialog(msg, title = title, easyClose = TRUE))
-}
-
-
-verify_password <- function(username, password, passwordfile) {
-  res <- system3("htpasswd", c("-bv", passwordfile, username, password),
-                 check = FALSE, output = FALSE)
-  res$success
-}
-
-
-read_groups <- function(groupfile) {
-  groups <- strsplit(readLines(groupfile), ":\\s+")
-  members <- strsplit(vcapply(groups, "[[", 2L), "\\s+")
-  names(members) <- vcapply(groups, "[[", 1L)
-  members
-}
-
-
-user_membership <- function(username, groupfile) {
-  groups <- read_groups(groupfile)
-  i <- vapply(groups, function(x) username %in% x, logical(1))
-  names(groups)[i]
-}
-
-
-read_users <- function(passwordfile) {
-  sub(":.*", "", readLines(passwordfile))
-}
-
-
-update_user_password <- function(username, password, passwordfile) {
-  hash <- twinkle::set_password(username, password)
-  prev <- readLines(passwordfile)
-  new <- sprintf("%s:%s", username, hash)
-  writeLines(c(prev[grepl("^username:", prev)], new),
-             passwordfile)
-}
-
-
-system3 <- function(command, args, check = FALSE, output = FALSE, env = NULL) {
-  if (output) {
-    code <- system2(command, args, stdout = "", stderr = "", env = env)
-    logs <- NULL
-  } else {
-    logs <- suppressWarnings(
-      system2(command, args, stdout = TRUE, stderr = TRUE, env = env))
-    code <- attr(logs, "status") %||% 0
-    attr(logs, "status") <- NULL
-  }
-
-  success <- code == 0L
-
-  if (check && !success) {
-    if (output) {
-      msg <- sprintf("Error code %d running command", code)
-    } else {
-      msg <- sprintf("Error code %d running command:\n%s", code,
-                     paste0("  > ", logs, collapse = "\n"))
-    }
-    stop(msg)
-  }
-
-  list(success = code == 0, code = code, output = logs)
-}
-
-
-set_vault_env <- function(vault_env_file) {
-  Sys.unsetenv(c("VAULTR_AUTH_METHOD", "VAULTR_CACHE_DIR"))
-  readRenviron(vault_env_file)
-}
-
-
-`%||%` <- function(a, b) {
-  if (is.null(a)) b else a
-}
-
-
-vcapply <- function(X, FUN, ...) {
-  vapply(X, FUN, "", ...)
-}
-
-
-squote <- function(x) {
-  sprintf("'%s'", x)
 }
 
 
