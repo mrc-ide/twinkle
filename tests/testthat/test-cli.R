c63dc12c9da42eb4aa1a14af11db878cc837603d
Wes Hinsley <w.hinsley@imperial.ac.uk>
2025-07-17 15:38:53 +0100

    Fix cli tests

--- tests/testthat/test-cli.R
+++ tests/testthat/test-cli.R
@@ -1,72 +1,44 @@
 test_that("cli_update_src args work", {
-  skip("rewrite")
   skip_if_not_installed("mockery")
   mock_run <- mockery::mock()
-  mockery::stub(cli_main, "twinkle_update_app", mock_run)
-  cli_main(args = c("update-src", "myapp", "--branch", "mybranch"))
+  mockery::stub(cli, "twinkle_update_src", mock_run)
+  cli(args = c("update-src", "myapp", "--branch", "mybranch"))
   mockery::expect_called(mock_run, 1)
   args <- mockery::mock_args(mock_run)[[1]]
   expect_equal(args, list(
-    name = "myapp", 
-    clone_repo = TRUE, 
-    install_packages = FALSE,
-    update_staging = FALSE,
-    update_production = FALSE, 
+    "myapp", 
     branch = "mybranch"))
 })
 
 
 test_that("cli_install_packages args work", {
-  skip("rewrite")
   skip_if_not_installed("mockery")
   mock_run <- mockery::mock()
-  mockery::stub(cli_main, "twinkle_update_app", mock_run)
-  cli_main(args = c("install-packages", "myapp"))
+  mockery::stub(cli, "twinkle_install_packages", mock_run)
+  cli(args = c("install-packages", "myapp"))
   mockery::expect_called(mock_run, 1)
   args <- mockery::mock_args(mock_run)[[1]]
-  expect_equal(args, list(
-    name = "myapp", 
-    clone_repo = FALSE, 
-    install_packages = TRUE,
-    update_staging = FALSE,
-    update_production = FALSE, 
-    branch = NULL))
+  expect_equal(args, list("myapp"))
 })
 
 
 test_that("cli_sync args work for staging", {
-  skip("rewrite")
   skip_if_not_installed("mockery")
   mock_run <- mockery::mock()
-  mockery::stub(cli_main, "twinkle_update_app", mock_run)
-  cli_main(args = c("sync", "myapp", "--staging"))
+  mockery::stub(cli, "twinkle_sync", mock_run)
+  cli(args = c("sync", "myapp", "--staging"))
   mockery::expect_called(mock_run, 1)
   args <- mockery::mock_args(mock_run)[[1]]
-  expect_equal(args, list(
-    name = "myapp", 
-    clone_repo = FALSE, 
-    install_packages = FALSE,
-    update_staging = TRUE,
-    update_production = FALSE, 
-    branch = NULL))
+  expect_equal(args, list("myapp", TRUE))
 })
 
 
 test_that("cli_sync args work for production", {
-  skip("rewrite")
   skip_if_not_installed("mockery")
   mock_run <- mockery::mock()
-  mockery::stub(cli_main, "twinkle_update_app", mock_run)
-  cli_main(args = c("sync", "myapp", "--production"))
+  mockery::stub(cli, "twinkle_sync", mock_run)
+  cli(args = c("sync", "myapp", "--production"))
   mockery::expect_called(mock_run, 1)
   args <- mockery::mock_args(mock_run)[[1]]
-  expect_equal(args, list(
-    name = "myapp", 
-    clone_repo = FALSE, 
-    install_packages = FALSE,
-    update_staging = FALSE,
-    update_production = TRUE, 
-    branch = NULL))
+  expect_equal(args, list("myapp", FALSE))
 })
-
-
